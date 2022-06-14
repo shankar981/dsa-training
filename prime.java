35a0db519f124b80d46baa15b4c6d24360947b64
shankar981 <107429758+shankar981@users.noreply.github.com>
2022-06-14 10:42:19 +0530

    primeornot

---
+++ prime.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Number= sc.nextInt();
+        int i;
+        for(i=2; i<=Math.sqrt(Number);i++)
+        {
+            if(Number%i==0)
+                break;
+        }
+        if(i==Number/2)
+            System.out.print(Number+" is prime");
+        else
+            System.out.print(Number+" is not prime");
+    }
+}
